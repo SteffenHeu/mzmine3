b3757157199dee52f3d6e064421d9274c36ecb47
SteffenHeu <steffen.heuckeroth@gmx.de>
2025-03-11 17:18:36 +0100

    refactor als implementation

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_baselinecorrection/als/ALSBaselineCorrectorParameters.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_baselinecorrection/als/ALSBaselineCorrectorParameters.java
@@ -11,11 +11,13 @@ public class ALSBaselineCorrectorParameters extends AbstractBaselineCorrectorPar
       "Smoothness parameter. Higher values make baseline more rigid (typically 1e5-1e9)",
       new DecimalFormat("#.##E0"), 1E5);
 
-  public static final DoubleParameter asymmetry = new DoubleParameter("Asymmetry", "Asymmetry parameter (typically 0.001-0.1)", new DecimalFormat("0.000"), 0.01);
+  public static final DoubleParameter asymmetry = new DoubleParameter("Asymmetry",
+      "Asymmetry parameter (typically 0.001-0.1)", new DecimalFormat("0.000"), 0.01, 0d, 1d);
 
-  public static final IntegerParameter iterations = new IntegerParameter("Iterations", "Number of iterations", 3);
+  public static final IntegerParameter iterations = new IntegerParameter("Iterations",
+      "Number of iterations", 3);
 
   public ALSBaselineCorrectorParameters() {
-    super(lambda, asymmetry, iterations, applyPeakRemoval.cloneParameter(), numSamples.cloneParameter());
+    super(lambda, asymmetry, iterations);
   }
 }
