022239ff1f7af706713450a0161efb9daffdd421
SteffenHeu <steffen.heuckeroth@gmx.de>
2025-08-11 14:41:15 +0200

    add ModuleSelectionRule

---
+++ mzmine-community/src/main/java/io/github/mzmine/modules/tools/batchwizard/postsetter/FirstModule.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2004-2025 The mzmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.mzmine.modules.tools.batchwizard.postsetter;
+
+import io.github.mzmine.modules.MZmineProcessingModule;
+import io.github.mzmine.modules.MZmineProcessingStep;
+import io.github.mzmine.modules.batchmode.BatchQueue;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+public final class FirstModule implements ModuleSelectionRule {
+
+  private final Class<? extends MZmineProcessingModule> module;
+
+  public FirstModule(Class<? extends MZmineProcessingModule> module) {
+    this.module = module;
+  }
+
+  @Override
+  public @NotNull List<@NotNull ? extends MZmineProcessingStep<?>> getSteps(BatchQueue q) {
+    return q.stream().filter(s -> module.isInstance(s.getModule())).findFirst().map(List::of)
+        .orElse(List.of());
+  }
+}
---
+++ mzmine-community/src/main/java/io/github/mzmine/modules/tools/batchwizard/postsetter/LastModule.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2004-2025 The mzmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.mzmine.modules.tools.batchwizard.postsetter;
+
+import io.github.mzmine.modules.MZmineProcessingModule;
+import io.github.mzmine.modules.MZmineProcessingStep;
+import io.github.mzmine.modules.batchmode.BatchQueue;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+public final class LastModule implements ModuleSelectionRule {
+
+  private final Class<? extends MZmineProcessingModule> module;
+
+  public LastModule(Class<? extends MZmineProcessingModule> module) {
+    this.module = module;
+  }
+
+  @Override
+  public @NotNull List<@NotNull ? extends MZmineProcessingStep<?>> getSteps(BatchQueue q) {
+    for (int i = q.size() - 1; i >= 0; i--) {
+      final MZmineProcessingStep<MZmineProcessingModule> s = q.get(i);
+      if (module.isInstance(s.getModule())) {
+        return List.of(s);
+      }
+    }
+    return List.of();
+  }
+}
