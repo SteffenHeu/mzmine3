9aff9d643674b46c610ee9762d4a9b21302729fc
Steffen Heuckeroth <steffen.heuckeroth@mzio.io>
2026-02-09 17:01:30 +0100

    sciex: merge errors

--- mzmine-community/src/main/java/io/github/mzmine/modules/visualization/spectra/simplespectra/SpectraPlot.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/visualization/spectra/simplespectra/SpectraPlot.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2025 The mzmine Development Team
+ * Copyright (c) 2004-2026 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -12,6 +12,7 @@
  *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -108,8 +109,6 @@ public class SpectraPlot extends EChartViewer implements LabelColorMatch {
    * If true, the labels of the data set will have the same color as the data set itself
    */
   protected BooleanProperty matchLabelColors;
-  private final ObjectProperty<MZTolerance> mzToleranceProperty = new SimpleObjectProperty<>();
-  private final ObjectProperty<Range<Double>> selectedMzRangeProperty = new SimpleObjectProperty<>();
 
   // Spectra processing
   protected DataPointProcessingController controller;
