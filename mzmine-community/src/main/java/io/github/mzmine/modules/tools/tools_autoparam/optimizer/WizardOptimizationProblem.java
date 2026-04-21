21294ce845fc75d1e2feacf4d49f94985ae29893
SteffenHeu <steffen.heuckeroth@gmx.de>
2026-04-21 16:14:32 +0200

    auto-param-2: throw except to terminate optimization if canceled

--- mzmine-community/src/main/java/io/github/mzmine/modules/tools/tools_autoparam/optimizer/WizardOptimizationProblem.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/tools/tools_autoparam/optimizer/WizardOptimizationProblem.java
@@ -70,6 +70,7 @@ import io.github.mzmine.parameters.parametertypes.tolerances.MZTolerance;
 import io.github.mzmine.parameters.parametertypes.tolerances.RTTolerance;
 import io.github.mzmine.project.ProjectService;
 import io.github.mzmine.taskcontrol.SimpleRunnableTask;
+import io.github.mzmine.taskcontrol.TaskStatus;
 import io.github.mzmine.util.CSVParsingUtils;
 import java.io.File;
 import java.io.IOException;
@@ -78,6 +79,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 import javafx.beans.property.SimpleStringProperty;
 import org.jetbrains.annotations.NotNull;
@@ -320,6 +322,12 @@ public class WizardOptimizationProblem extends AbstractProblem {
       }
     }
 
+    if(batchTask.isCanceled()) {
+      // we need to throw here, because the the optimizer does not respect
+      // the termination condition with the task status
+      throw new RuntimeException("Batch optimization task was canceled");
+    }
+
     final FeatureList newest = project.getCurrentFeatureLists().stream()
         .max(Comparator.comparing(f -> f.getName().length())).get();
 
@@ -340,7 +348,7 @@ public class WizardOptimizationProblem extends AbstractProblem {
     solution.setAttribute("Total features", newest.streamFeatures().count());
     solution.setAttribute("Runtime / s", fullBatchTime);
 
-    project.removeFeatureLists(project.getCurrentFeatureLists());
+    project.removeFeatureLists(batchTask.getLatestCreatedFeatureLists());
   }
 
   public void applyBatchOverridesToSequence(@NotNull Solution solution,
